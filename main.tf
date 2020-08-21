e0889efacfd4f37a0e51475812f984a272eb0c7b
Damian Sloane <damian@capacitorlabs.com>
2020-08-21 13:49:25 +1000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,70 @@
+locals {
+  database_id = replace(var.name, "_", "-")
+  database_subnet_group_name = "${local.database_id}-database-subnet-group"
+  database_cluster_group_name = "${local.database_id}-database-cluster-group"
+  database_cluster_parameter_group_name = "${local.database_id}-database-cluster-parameter-group"
+}
+
+# Create subnet group
+resource "aws_db_subnet_group" "subnet_group" {
+  name = var.name
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name = var.name
+  }
+}
+
+# Create parameter group
+resource "aws_rds_cluster_parameter_group" "cluster_parameter_group" {
+  name = var.name
+  family = var.family
+
+  dynamic "parameter" {
+    for_each = var.database_cluster_parameters
+    content {
+      name = parameter.value["name"]
+      value = parameter.value["value"]
+      apply_method = parameter.value["apply_method"]
+    }
+  }
+
+  tags = {
+    Name = var.name
+  }
+}
+
+# Create random password for database master user
+resource "random_password" "master_user_password" {
+  length = 32
+  special = false
+}
+
+# Create database cluster
+resource "aws_rds_cluster" "database_cluster" {
+  snapshot_identifier = var.snapshot_identifier
+  engine_mode = "serverless"
+  engine = var.engine
+  engine_version = var.engine_version
+  backup_retention_period = var.backup_retention_period
+  cluster_identifier = var.name
+  database_name = var.name
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_parameter_group.name
+  db_subnet_group_name = aws_db_subnet_group.subnet_group.name
+  master_username = var.master_username
+  master_password = random_password.master_user_password.result
+  skip_final_snapshot = var.skip_final_snapshot
+  storage_encrypted = var.storage_encrypted
+  vpc_security_group_ids = var.security_group_ids
+  # Configure scaling policy for serverless database
+  scaling_configuration {
+    auto_pause               = var.auto_pause
+    max_capacity             = var.max_capacity
+    min_capacity             = var.min_capacity
+    seconds_until_auto_pause = var.auto_pause_seconds
+    timeout_action           = "ForceApplyCapacityChange"
+  }
+  tags = {
+    Name = var.name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,109 @@
+variable "name" {
+  description = "Name of the database (must be MySQL compliant)"
+  type = string
+}
+
+variable "security_group_ids" {
+  description = "Set of AWS security group IDs to link to the database"
+  type = set(string)
+}
+
+variable "snapshot_identifier" {
+  description = "Optional snapshot identifier to use when provisioning the database"
+  type = string
+  default = null
+}
+
+variable "subnet_ids" {
+  description = "Set of AWS subnet IDs in which the database will be provisioned"
+  type = set(string)
+}
+
+variable "master_username" {
+  description = "Database master username"
+  type = string
+}
+
+variable "master_password" {
+  description = "Database master password"
+  type = string
+}
+
+variable "backup_retention_period" {
+  description = "Backup retention period (number of days)"
+  type = number
+  default = 30
+}
+
+variable "skip_final_snapshot" {
+  description = "Boolean flag, if true will skip final snapshot on database deletion"
+  type = bool
+  default = true
+}
+
+variable "storage_encrypted" {
+  description = "Boolean flag, if true the contents of the database will be encrypted at rest"
+  type = bool
+  default = true
+}
+variable "database_parameters" {
+  description = "Database parameters to set"
+  type = list(object({
+    name = string
+    value = number
+    apply_method = string
+  }))
+  default = []
+}
+
+variable "database_cluster_parameters" {
+  description = "Database cluster parameters to set"
+  type = list(object({
+    name = string
+    value = number
+    apply_method = string
+  }))
+  default = []
+}
+
+variable "auto_pause" {
+  description = "Boolean flag, if true database will be paused automatically after a period of inactivity"
+  type = bool
+  default = false
+}
+
+variable "auto_pause_seconds" {
+  description = "Seconds of inactivity before database is paused"
+  type = number
+  default = 600
+}
+
+variable "min_capacity" {
+  description = "Database scaling minimum capacity"
+  type = number
+  default = 1
+}
+
+variable "max_capacity" {
+  description = "Database scaling maximum capacity"
+  type = number
+  default = 2
+}
+
+variable "engine" {
+  description = "RDS database engine"
+  type = string
+  default = "aurora-mysql"
+}
+
+variable "engine_version" {
+  description = "RDS database engine version"
+  type = string
+  default = "5.7.mysql_aurora.2.07.1"
+}
+
+variable "family" {
+  description = "RDS database family"
+  type = string
+  default = "aurora-mysql5.7"
+}
